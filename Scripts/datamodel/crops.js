26dadc6d641d558b3f9019b5036302e47552ff49
priyanshu kumar <84118213+priyanshu-k1@users.noreply.github.com>
2025-01-13 13:35:16 +0530

    added new js files

---
+++ Scripts/datamodel/crops.js
@@ -0,0 +1,130 @@
+class Rice {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Wheat {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Lentils {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Cotton {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Maize {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class SugarCane {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Soybeans {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Groundnuts {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Millets {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
+  class Coffee {
+    BestSeasonToGrow = "";
+    Duration = "";
+    Soil = "";
+    WaterRequirements = "";
+    Irrigation = "";
+    Temperature = 0.0;
+    Fertilizers = "";
+    Pesticides = "";
+    Pests = "";
+    Infections = "";
+  }
+  
---
+++ Scripts/language.js
@@ -0,0 +1,32 @@
+
+
+class English{
+
+}
+class Hindi{
+    
+}
+class Telugu{
+    
+}
+
+
+
+function selectLanguage(lang) {
+    localStorage.setItem('selectedLanguage', lang);
+    alert(`Language selected: ${lang}`);
+  }
+
+
+
+function changeLanguage(lang){
+    if(lang=="hindi"){
+
+    }
+    else if(lang="telugu"){
+
+    }
+    else{
+        
+    }
+}
